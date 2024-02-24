3653d5f1dbd00daba4de8b2c4c55ead4bb7b5637
Thanatuch <thanatuch.2003@gmail.com>
2024-02-24 14:32:22 +0700

    add reset password with email

--- app/src/main/java/com/android/burgerking1112app/ForgetPasswordActivity.kt
+++ app/src/main/java/com/android/burgerking1112app/ForgetPasswordActivity.kt
@@ -2,9 +2,14 @@ package com.android.burgerking1112app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.burgerking1112app.databinding.ActivityForgetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ForgetPasswordActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private val view: ActivityForgetPasswordBinding by lazy { ActivityForgetPasswordBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +19,23 @@ class ForgetPasswordActivity : AppCompatActivity() {
             finish();
         }
 
+        auth = Firebase.auth
+
         view.btnSubmitEmail.setOnClickListener {
-            var email = view.tvResetEmail.text
+            var email = view.tvResetEmail.text.toString()
+
+            if (email != ""){
+                auth.sendPasswordResetEmail(email).addOnCompleteListener{task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(
+                            this,
+                            "Email sent successfully to reset your password",
+                            Toast.LENGTH_SHORT,
+                        )
+                        finish()
+                    }
+                }
+            }
         }
     }
 
