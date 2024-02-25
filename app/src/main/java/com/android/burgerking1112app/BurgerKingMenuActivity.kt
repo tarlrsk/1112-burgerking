afa54eaa18d68c94af7c874b5bc5044c0e90865c
Thanatuch <thanatuch.2003@gmail.com>
2024-02-25 13:48:59 +0700

    add firebase to promo menu

--- app/src/main/java/com/android/burgerking1112app/BurgerKingMenuActivity.kt
+++ app/src/main/java/com/android/burgerking1112app/BurgerKingMenuActivity.kt
@@ -1,26 +1,59 @@
 package com.android.burgerking1112app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.HorizontalScrollView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.burgerking1112app.adapters.PromoAdapter
 import com.android.burgerking1112app.databinding.ActivityBurgerKingMenuBinding
 import com.android.burgerking1112app.models.PromoMenu
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
+import com.google.firebase.firestore.firestore
 
 class BurgerKingMenuActivity : AppCompatActivity() {
     private val view: ActivityBurgerKingMenuBinding by lazy { ActivityBurgerKingMenuBinding.inflate(layoutInflater) }
-    private val promos = listOf(
-        PromoMenu("Buy 1 Get 1 Free : Mushroom Swiss (Beef) + Regular French Fries", "฿ 258", "https://firebasestorage.googleapis.com/v0/b/burgerking1112app.appspot.com/o/hatyai_promo.png?alt=media&token=46b27b8c-919e-4f0a-8f0a-e4d5875cf140"),
-        PromoMenu("Hat Yai Chicken King 2 pcs + 1 sticky Rice + Drink 16 oz 1 cup", "฿ 129", "https://firebasestorage.googleapis.com/v0/b/burgerking1112app.appspot.com/o/mushroom_promo.png?alt=media&token=1cf41754-b678-4ce3-9b83-f7b1eeba9e6d"),
-        PromoMenu("Hat Yai Chicken King 2 pcs + 1 sticky Rice + Drink 16 oz 1 cup", "฿ 129", "https://firebasestorage.googleapis.com/v0/b/burgerking1112app.appspot.com/o/mushroom_promo.png?alt=media&token=1cf41754-b678-4ce3-9b83-f7b1eeba9e6d")
-    )
+    private val database = Firebase.database("https://burger-king-1112-app-9e090-default-rtdb.asia-southeast1.firebasedatabase.app")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
 
 
-        view.rvHorizontalMenu.adapter = PromoAdapter(this, promos)
-        view.rvHorizontalMenu.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+
+        var promos = arrayListOf<PromoMenu>()
+
+        val promosRef = database.reference.child("promos")
+
+        promosRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                promos.clear()
+                if(snapshot.exists()){
+                    for(promoSnap in snapshot.children){
+                        val promoData = promoSnap.getValue(PromoMenu::class.java)
+                        Log.d(TAG, "Some logging "+ promoData?.promoDescription.toString() + promoData?.imgPath.toString());
+                        promos.add(promoData!!)
+                    }
+                }
+
+                view.rvHorizontalMenu.adapter = PromoAdapter(this@BurgerKingMenuActivity, promos)
+                view.rvHorizontalMenu.layoutManager = LinearLayoutManager(this@BurgerKingMenuActivity, LinearLayoutManager.HORIZONTAL, false)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
     }
+
 }
