c19f454b740634a680587a08a075e04398117056
Thanatuch <thanatuch.2003@gmail.com>
2024-02-26 13:43:32 +0700

    add to to cart

--- app/src/main/java/com/android/burgerking1112app/adapters/PromoAdapter.kt
+++ app/src/main/java/com/android/burgerking1112app/adapters/PromoAdapter.kt
@@ -3,17 +3,18 @@ package com.android.burgerking1112app.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.android.burgerking1112app.databinding.HorizonPromoMenuItemBinding
+import com.android.burgerking1112app.models.CartItem
 import com.android.burgerking1112app.models.PromoMenu
 import com.bumptech.glide.Glide
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 
 
 
 
-class PromoAdapter(private val context: Context, private val promos: List<PromoMenu>)
+class PromoAdapter(private val context: Context, private val promos: List<PromoMenu>,private val firebaseDatabase: FirebaseDatabase, private val userId: String)
     : RecyclerView.Adapter<PromoAdapter.RecyclerViewHolder>() {
     class RecyclerViewHolder(val binding: HorizonPromoMenuItemBinding): RecyclerView.ViewHolder(binding.root)
 
@@ -42,5 +43,11 @@ class PromoAdapter(private val context: Context, private val promos: List<PromoM
         Glide.with(context).load(storageRef).into(holder.binding.imgPromo)
         holder.binding.tvPromoDescription.text = promo.promoDescription.toString()
         holder.binding.tvPromoPrice.text = "฿ " + promo.promoPrice.toString()
+
+        holder.binding.btnPromoSelect.setOnClickListener  {
+            val cartRef = firebaseDatabase.reference.child("carts").child(userId).push()
+            val item = CartItem(cartRef.key,promo.promoDescription, promo.promoPrice, promo.imgPath, 1)
+            cartRef.setValue(item)
+        }
     }
 }
---
+++ app/src/main/java/com/android/burgerking1112app/models/CartItem.kt
@@ -0,0 +1,9 @@
+package com.android.burgerking1112app.models
+
+data class CartItem(
+    val id: String? = null,
+    val name: String? = null,
+    val price: Long? = null,
+    val imgPath: String? = null,
+    val qty: Int
+)
