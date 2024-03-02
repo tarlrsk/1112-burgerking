36819a7b10ba4c9c46d5c59fd549e8c06cf25300
Sorracha <109853255+beausorracha@users.noreply.github.com>
2024-03-02 13:59:55 +0700

    update fragment orders

--- app/src/main/java/com/android/burgerking1112app/SelectAddressActivity.kt
+++ app/src/main/java/com/android/burgerking1112app/SelectAddressActivity.kt
@@ -1,6 +1,5 @@
 package com.android.burgerking1112app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
--- app/src/main/java/com/android/burgerking1112app/fragments/FragmentOrders.kt
+++ app/src/main/java/com/android/burgerking1112app/fragments/FragmentOrders.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.burgerking1112app.adapters.AddressAdapter
+import com.android.burgerking1112app.adapters.OrderHistoryAdapter
 import com.android.burgerking1112app.databinding.FragmentOrdersBinding
+import com.android.burgerking1112app.models.Address
+import com.android.burgerking1112app.models.OrderHistory
 
 class FragmentOrders: Fragment() {
     private lateinit var view: FragmentOrdersBinding
+
+    private val orders = listOf<OrderHistory>(
+        OrderHistory("29 DEC 2023 12:27", "Arrived", "2041986341", "Assumption University")
+    )
 
     // Map UI with fragment
     override fun onCreateView(
@@ -19,5 +28,9 @@ class FragmentOrders: Fragment() {
         view = FragmentOrdersBinding.inflate(inflater, container, false)
 
         return view.root
+
+        view.rvHistoryOrders.adapter = OrderHistoryAdapter(requireContext(), orders)
+        view.rvHistoryOrders.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
     }
 }
